3435cd57a9e3efd84339d6f78fb0390ba11cac5f
Eric <eric@canva.com>
2026-02-02 11:28:31 +1100

    unit tests

--- config/vite/vite.config.ts
+++ config/vite/vite.config.ts
@@ -1,6 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './setup_tests.ts',
+    css: true,
+    include: ['../../tests/**/*.test.{ts,tsx}'],
+  },
 })
